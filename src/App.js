9bdfe0d5146ca7dc01e7b4c9bb8047f8b7a81f0d
maria-badanina <maria.badanina.s@gmail.com>
2019-02-01 18:24:30 +0100

    Create Experience component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
-import sample from './services/sample.json';
-import sample_2 from './services/sample_2.json';
-import Preview from './components/Preview';
+import React, { Component } from "react";
+import "./App.css";
+import sample from "./services/sample.json";
+import sample_2 from "./services/sample_2.json";
+import Preview from "./components/Preview";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       sample: sample_2
-    }
+    };
   }
 
-
   render() {
-    const {sample} = this.state;
-    const {profile} = sample.author;
+    const { sample } = this.state;
     return (
       <div className="App">
         <h1>Manfred Awesomic CV</h1>
-        <Preview />
-        <ul>
-            {profile.map((item, index)=>{
-              return(
-                <li key={index}>
-                <h2>{item.name}</h2>
-                <h2>{item.yearsOfExperience}</h2>
-                </li>
-              );
-            })}
-        </ul>
+        <Preview sample={sample} />
       </div>
     );
   }

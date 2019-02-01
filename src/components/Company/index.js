671a51bb63f8aafdfa54e42b5f17fa3f6337b076
maria-badanina <maria.badanina.s@gmail.com>
2019-02-01 20:28:25 +0100

    Create RoleList element

--- src/components/Company/index.js
+++ src/components/Company/index.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import RoleList from '../RoleList';
 
 class Company extends Component {
 
     render() {
         const {company} = this.props.experience;
         return (
-            <li>
+            <li className="company__item">
             {company.map((item, index)=>{
                 return (
-                    <p key={index}>Nombre de la compañía: {item.name}</p>
-
+                  <Fragment key={index}>
+                    <p className="company__name">Nombre de la compañía: {item.name}</p>
+                    <RoleList  roles={item.roles}/>
+                  </Fragment>
                 );
             })}
             </li>

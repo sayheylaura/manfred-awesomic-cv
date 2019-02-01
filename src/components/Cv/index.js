ba1acdd90009da2fc5844092b21e3cc233a5ccc8
lauraferrandof <lauraferrandof@gmail.com>
2019-02-01 18:36:13 +0100

    Create Author component

--- src/components/Cv/index.js
+++ src/components/Cv/index.js
@@ -1,24 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Author from "../Author";
 
 class Cv extends Component {
-
-    render() { 
-        return ( 
-        // Importado en:
-        //     - Preview
-        <div className="cv__wrapper">
-         {/* Instancia:
-             - Author
-             - Experience
-             - Education
-             - Languages
-             - MiscEducation
-             - Questionnaire */}
-             <p>Cv component goes here</p>
-
-        </div>
-        );
-    }
+  render() {
+    const { sample } = this.props;
+    return (
+      // Importado en:
+      //     - Preview
+      <div className="cv__wrapper">
+        {/* Instancia:
+          - Author
+          - Experience
+          - Education
+          - Languages
+          - MiscEducation
+          - Questionnaire */}
+        <Author sampleAuthor={sample.author} />
+      </div>
+    );
+  }
 }
+
+Cv.propTypes = {
+  sample: PropTypes.object.isRequired
+};
 
 export default Cv;

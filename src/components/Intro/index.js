fbe6b2cfa4cee72431578a3ad554144787b9ded4
emma-martin <emmamartinalvarez@gmail.com>
2019-02-03 11:07:02 +0100

    add title to intro

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -4,6 +4,9 @@ import PropTypes from "prop-types";
 const Intro = ({sampleIntro}) => {
   return (
     <div className="intro__wrapper">
+    <h3 className="intro__wrapper">
+      INTRO
+    </h3>
       <p className="intro__paragraph">{sampleIntro}</p>
     </div>
   );

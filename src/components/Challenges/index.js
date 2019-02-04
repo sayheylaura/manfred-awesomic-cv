95a71f25b987456e9eae941b99df810879b04560
minamrp <ramosp.marina@gmail.com>
2019-02-04 19:12:38 +0100

    complete component

--- src/components/Challenges/index.js
+++ src/components/Challenges/index.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ChallengesActions from '../ChallengesActions';
 
 class Challenges extends Component {
   render() {
     const { challenges } = this.props;
-    console.log(challenges);
     return (
       <ul className="challenges__list">
         {challenges.map((item, index )=> {
           return (
             <li  key={index} className="challenges__item">
             <p className="challenges__item-summary">{item.summary}</p>
-
+              <ChallengesActions challengesActions={item.actions}/>
             </li>
           );
         })}

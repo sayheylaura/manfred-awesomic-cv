c62115b0af36e58ed5d40da642ba49158aa362c3
minamrp <ramosp.marina@gmail.com>
2019-02-04 20:46:19 +0100

    indent

--- src/components/RelationshipItem/index.js
+++ src/components/RelationshipItem/index.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 
 class RelationshipItem extends Component {
   render() {
-    const { name, comment, role, contact, company } = this.props.relation;
+    const { name,
+            comment,
+            role,
+            contact,
+            company
+          } = this.props.relation;
     return (
       <li className="sig-relations__item">
         <p className="sig-relations__name-comment">{`${name}, ${comment}`}</p>

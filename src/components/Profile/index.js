fdf2b9bc3c44f76907e6aea2a2629909fafaacf0
minamrp <ramosp.marina@gmail.com>
2019-02-04 20:18:26 +0100

    conditional rendering

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -28,8 +28,9 @@ class Profile extends Component {
               <p className="author__yearsOfExperience">
                 {item.yearsOfExperience} years of experience
               </p>
-
-              <PublicLinks profileLinks={item.publicLinks} />
+              {item.publicLinks && (
+                <PublicLinks profileLinks={item.publicLinks} />
+              )}
             </Fragment>
           );
         })}

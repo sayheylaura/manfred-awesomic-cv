b724b3a95c6b05e8dd66aca59ae8a13d40755053
minamrp <ramosp.marina@gmail.com>
2019-02-05 15:46:50 +0100

    example.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 //import sample from "./services/sample.json";
-import sample_2 from "./services/sample_2.json";
+import example from "./services/example.json";
+//import sample_2 from "./services/sample_2.json";
 import Preview from "./components/Preview";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sample: sample_2
+      sample: example
     };
   }
 

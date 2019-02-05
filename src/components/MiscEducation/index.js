71a6192881fb09459112ddbc1bb8bcd2c9adc2cf
lauraferrandof <lauraferrandof@gmail.com>
2019-02-05 10:37:39 +0100

    MiscEducation styles

--- src/components/MiscEducation/index.js
+++ src/components/MiscEducation/index.js
@@ -1,22 +1,22 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MiscEducation extends Component {
   render() {
     const { sampleMiscEducation } = this.props;
     return (
-      <Fragment>
-        <h3>Miscellany</h3>
+      <div className="miscEducation__wrapper">
+        <h2 className="miscEducation__title">Miscellany</h2>
         <ul className="miscEducation__list">
           {sampleMiscEducation.map((item, index) => {
-            return(
-            <li key={index} className="miscEducation__item">
-              {item}
-            </li>
+            return (
+              <li key={index} className="miscEducation__item">
+                {item}
+              </li>
             );
           })}
         </ul>
-      </Fragment>
+      </div>
     );
   }
 }

0ce7f62464a6c85e7338917f0ad24c23ef3530b2
emma-martin <emmamartinalvarez@gmail.com>
2019-02-07 13:24:18 +0100

    working router, needs refinement

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+
 import PropTypes from "prop-types";
 import Preview from "../Preview";
 import Form from "../Form";
@@ -8,11 +10,20 @@ class Main extends Component {
   render() {
     const { sample, handlePrintBtn, handleJsonText } = this.props;
     return (
+      <Fragment>
+      <div className="links">
+          <Link to={`/form`} className="link">Form</Link>
+          <Link to={`/json`} className="link">Json</Link>
+          <Link to={`/preview`} className="link">Cv</Link>
+        </div>
       <main className="main">
-        <Form />
-        <Json sample={sample} handleJsonText={handleJsonText} />
-        <Preview sample={sample} handlePrintBtn={handlePrintBtn} />
+        <Switch>
+          <Route path="/form" render={()=><Form />}/>
+          <Route path="/json" render={()=><Json sample={sample} handleJsonText={handleJsonText} />}/>
+          <Route path="/preview" render={()=><Preview sample={sample} handlePrintBtn={handlePrintBtn} />}/>
+        </Switch>
       </main>
+      </Fragment>
     );
   }
 }

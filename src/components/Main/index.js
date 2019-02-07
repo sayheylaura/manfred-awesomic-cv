2b084e2621557f51e1ecd9eb97c0fb9824cd9a9b
lauraferrandof <lauraferrandof@gmail.com>
2019-02-07 10:19:29 +0100

    Create form component. Instantiate in Main component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Preview from "../Preview";
+import Form from "../Form";
 
 class Main extends Component {
   render() {
     const { sample, handlePrintBtn } = this.props;
     return (
       <main className="main">
+        <Form />
         <Preview sample={sample} handlePrintBtn={handlePrintBtn} />
       </main>
     );

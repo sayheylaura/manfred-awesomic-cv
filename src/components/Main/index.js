4560e1f935e4f789d06592ac7cafaf6883e0e718
lauraferrandof <lauraferrandof@gmail.com>
2019-02-07 18:22:36 +0100

    Fix main route

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { Route, Switch, Link } from "react-router-dom";
 
 import PropTypes from "prop-types";
@@ -10,23 +10,20 @@ class Main extends Component {
   render() {
     const { sample, handlePrintBtn, handleJsonText } = this.props;
     return (
-      <Fragment>
-
       <main className="main">
-      <nav className="main__nav">
-        <ul className="nav__list">
-          <Link to="/form"><li className="nav__link">Form</li></Link>
-          <Link to="/json"><li className="nav__link">Json</li></Link>
-          <Link to="/preview"><li className="nav__link">Cv</li></Link>
-        </ul>
-      </nav>
+        <nav className="main__nav">
+          <ul className="nav__list">
+            <Link to="/"><li className="nav__link">Form</li></Link>
+            <Link to="/json"><li className="nav__link">Json</li></Link>
+            <Link to="/preview"><li className="nav__link">Cv</li></Link>
+          </ul>
+        </nav>
         <Switch>
-          <Route path="/form" render={()=><Form />}/>
-          <Route path="/json" render={()=><Json sample={sample} handleJsonText={handleJsonText} />}/>
-          <Route path="/preview" render={()=><Preview sample={sample} handlePrintBtn={handlePrintBtn} />}/>
+          <Route exact path="/" render={() => <Form />} />
+          <Route path="/json" render={() => <Json sample={sample} handleJsonText={handleJsonText} />} />
+          <Route path="/preview" render={() => <Preview sample={sample} handlePrintBtn={handlePrintBtn} />} />
         </Switch>
       </main>
-      </Fragment>
     );
   }
 }

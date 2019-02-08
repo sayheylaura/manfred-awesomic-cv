486a99caba0401f3f8633df7dadeac66586a3e4d
lauraferrandof <lauraferrandof@gmail.com>
2019-02-08 10:04:17 +0100

    Create FormQuestionnaire component and logic to paint its items

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-//import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import FormQuestionnaire from "../FormQuestionnaire";
 
 class Form extends Component {
   render() {
+    const { sample } = this.props;
+    const { questionnaire } = sample;
     return (
       <section className="form__wrapper">
         <h2 className="form__title">Here will be the form</h2>
+
+        <FormQuestionnaire questionnaire={questionnaire} />
       </section>
     );
   }
 }
+
+Form.propTypes = {
+  sample: PropTypes.object.isRequired
+};
 
 export default Form;

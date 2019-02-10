f79077e7ea17642b85d2ae939bbe3f6c368d6943
lauraferrandof <lauraferrandof@gmail.com>
2019-02-10 19:43:26 +0100

    Create FormMiscEducationItem component. Create functions to add and remove items. Create functions to control inputs change

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,20 +4,39 @@ import FormMiscEducation from "../FormMiscEducation";
 
 class Form extends Component {
   render() {
-    const { sample } = this.props;
+    const {
+      sample,
+      miscEdDefault,
+      handleDefaultInputChange,
+      handleAddMiscItem,
+      handleRemoveMiscItem,
+      handleMiscInputChange
+    } = this.props;
     const { miscEducation } = sample;
     return (
       <section className="form__wrapper">
         <h2 className="form__title">Here will be the form</h2>
 
-        <FormMiscEducation miscEducation={miscEducation} />
+        <FormMiscEducation
+          miscEducation={miscEducation}
+          miscEdDefault={miscEdDefault}
+          handleDefaultInputChange={handleDefaultInputChange}
+          handleAddMiscItem={handleAddMiscItem}
+          handleRemoveMiscItem={handleRemoveMiscItem}
+          handleMiscInputChange={handleMiscInputChange}
+        />
       </section>
     );
   }
 }
 
 Form.propTypes = {
-  sample: PropTypes.object.isRequired
+  sample: PropTypes.object.isRequired,
+  miscEdDefault: PropTypes.string.isRequired,
+  handleDefaultInputChange: PropTypes.func.isRequired,
+  handleAddMiscItem: PropTypes.func.isRequired,
+  handleRemoveMiscItem: PropTypes.func.isRequired,
+  handleMiscInputChange: PropTypes.func.isRequired
 };
 
 export default Form;
--- src/components/FormMiscEducation/index.js
+++ src/components/FormMiscEducation/index.js
@@ -1,38 +1,71 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import FormInput from '../FormInput';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import FormMiscEducationItem from "../FormMiscEducationItem";
+import FormInput from "../FormInput";
+import Button from "../Button";
 
 class FormMiscEducation extends Component {
   render() {
-    const { miscEducation } = this.props;
-    console.log(miscEducation);
+    const {
+      miscEducation,
+      miscEdDefault,
+      handleDefaultInputChange,
+      handleAddMiscItem,
+      handleRemoveMiscItem,
+      handleMiscInputChange
+    } = this.props;
     return (
       <fieldset className="form__misc-education">
         <legend className="form__section-title">Miscellany</legend>
 
-        <p className="form__section-description">Miscellaneous education such as courses, certifications, awards, projects, etc.</p>
+        <p className="form__section-description">
+          Miscellaneous education such as courses, certifications, awards,
+          projects, etc.
+        </p>
 
         {miscEducation.map((item, index) => {
           return (
-            <FormInput
+            <FormMiscEducationItem
               key={index}
-              labelContent={`Item ${index + 1}`}
-              styles="form__input"
-              inputType="text"
-              inputName={`miscEd${index}`}
-              inputValue={item}
-              example="Scrum master certification"
-            /* handleInputChange={handleProfileInputs} */
+              ind={index}
+              item={item}
+              handleRemoveMiscItem={handleRemoveMiscItem}
+              handleMiscInputChange={handleMiscInputChange}
             />
-          )
+          );
         })}
+
+        <div>
+          <FormInput
+            labelContent="Item"
+            styles="form__input"
+            inputType="text"
+            inputName="miscEdDefault"
+            inputValue={miscEdDefault}
+            example="Scrum master certification"
+            handleInputChange={handleDefaultInputChange}
+          />
+
+          <Button
+            buttonType="button"
+            styles="add-btn"
+            handleButtonClick={handleAddMiscItem}
+          >
+            Add item
+          </Button>
+        </div>
       </fieldset>
     );
   }
 }
 
 FormMiscEducation.propTypes = {
-  miscEducation: PropTypes.array.isRequired
-}
+  miscEducation: PropTypes.array.isRequired,
+  miscEdDefault: PropTypes.string.isRequired,
+  handleDefaultInputChange: PropTypes.func.isRequired,
+  handleAddMiscItem: PropTypes.func.isRequired,
+  handleRemoveMiscItem: PropTypes.func.isRequired,
+  handleMiscInputChange: PropTypes.func.isRequired
+};
 
 export default FormMiscEducation;

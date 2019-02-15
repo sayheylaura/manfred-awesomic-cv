a2406cfb02405c610f5a2404fb3b66525f46b729
lauraferrandof <lauraferrandof@gmail.com>
2019-02-15 20:30:49 +0100

    Finish logic to control company items

--- src/components/FormExperienceItem/index.js
+++ src/components/FormExperienceItem/index.js
@@ -1,51 +1,26 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import FormInput from "../FormInput";
-import Button from "../Button";
+import FormCompany from "../FormCompany";
 
 class FormExperienceItem extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleRemoveBtn = this.handleRemoveBtn.bind(this);
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleRemoveBtn() {
-    const { handleRemoveExperienceItem, ind } = this.props;
-    handleRemoveExperienceItem(ind);
-  }
-
-  handleInputChange(event) {
-    const { value, name } = event.currentTarget;
-    const { handleExperienceChange, ind } = this.props;
-    handleExperienceChange(value, name, ind);
-  }
-
   render() {
-    const { company } = this.props.item;
+    const {
+      item,
+      handleRemoveExperienceItem,
+      handleExperienceChange
+    } = this.props;
+    const { company } = item;
     return (
       <div>
         {company.map((item, index) => {
           return (
-            <Fragment key={index}>
-              <FormInput
-                labelContent="Company name"
-                styles="form__input"
-                inputType="text"
-                inputName="name"
-                inputValue={item.name}
-                example="Ex: Google"
-                handleInputChange={this.handleInputChange}
-              />
-              <Button
-                buttonType="button"
-                styles="remove-btn"
-                handleButtonClick={this.handleRemoveBtn}
-              >
-                Remove
-              </Button>
-            </Fragment>
+            <FormCompany
+              key={index}
+              ind={index}
+              name={item.name}
+              handleRemoveExperienceItem={handleRemoveExperienceItem}
+              handleExperienceChange={handleExperienceChange}
+            />
           );
         })}
       </div>
